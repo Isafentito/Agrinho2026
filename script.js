415975502c966fc7ad264048d63f9de98695d4e2
Isafentito <isadora.fenato@escola.pr.gov.br>
2026-07-07 18:35:34 -0300

    .

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ function mudarInfo(opcao) {
 
     if (opcao === 1) {
         titulo.innerText = "O que é?";
-        paragrafo.innerText = "Aqui você digita o texto explicativo sobre o primeiro assunto do seu site. Pode ser um parágrafo longo ou curto.";
-    } 
+        paragrafo.innerText = "Os diferentes meios agrícolas são o conjunto de técnicas, métodos, sistemas e tecnologias utilizados pelo ser humano para cultivar plantas e produzir alimentos, matérias-primas ou outros produtos vegetais.Em termos simples, eles representam as diferentes formas e caminhos de se praticar a agricultura. Em vez de depender apenas do método tradicional (plantar diretamente na terra comum do chão), esses meios englobam todas as alternativas que a ciência e a criatividade humana desenvolveram para adaptar o cultivo a diferentes situações — como a falta de espaço, climas difíceis, escassez de água ou solos que perderam seus nutrientes." } 
     else if (opcao === 2) {
         titulo.innerText = "Quais são os diferentes meios?";
-        paragrafo.innerText = "Este é o espaço da segunda informação. Basta apagar este texto dentro das aspas e escrever o que você quiser.";
+        paragrafo.innerText =;
     } 
     else if (opcao === 3) {
         titulo.innerText = "Como fazer?";
