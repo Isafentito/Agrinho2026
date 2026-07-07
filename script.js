d9283f87852285dcb4d3e86a47b3afc15ff57a1f
Isafentito <isadora.fenato@escola.pr.gov.br>
2026-07-07 20:50:32 -0300

    .

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ function mudarInfo(opcao) {
         titulo.innerText = "O que é?";
         paragrafo.innerText = "Os diferentes meios agrícolas são o conjunto de técnicas, métodos, sistemas e tecnologias utilizados pelo ser humano para cultivar plantas e produzir alimentos, matérias-primas ou outros produtos vegetais. Em termos simples, eles representam as diferentes formas e caminhos de se praticar a agricultura. Em vez de depender apenas do método tradicional (plantar diretamente na terra comum do chão), esses meios englobam todas as alternativas que a ciência e a criatividade humana desenvolveram para adaptar o cultivo a diferentes situações — como a falta de espaço, climas difíceis, escassez de água ou solos que perderam seus nutrientes.";
     } 
+
     else if (opcao === 2) {
-        titulo.innerText = "Quais são os diferentes meios?";
-        paragrafo.innerText = `Cultivo em Substrato. 
-Hidroponia.
-Aquaponia.
-Agricultura Vertical.
-Agrofloresta.`;
+        titulo.innerText = 'Quais são os diferentes meios?';
+        paragrafo.innerText = "Cultivo em Substrato." 
+"Hidroponia."
+"Aquaponia."
+"Agricultura Vertical."
+"Agrofloresta.";
     } 
+
     else if (opcao === 3) {
         titulo.innerText = "Como fazer?";
         paragrafo.innerText = `Como fazer Cultivo em Substrato: Consiste em escolher recipientes como vasos, baldes ou caixotes de feira e preenchê-los com uma mistura rica de terra preta, adubo orgânico (compostagem) e fibra de coco. Dessa forma, as raízes crescem protegidas e bem alimentadas, sem nunca entrar em contato com o solo infértil do chão.
